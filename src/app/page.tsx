654ddd24719c78aaac61977212e9495a5021a7c1
Kevin Lu <kevinlu.email@gmail.com>
2025-04-13 20:47:31 -0700

    style the email properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="font-serif">hi im kevin. more is coming.</h1>
-      <p>
-        message me at <a href="mailto:me@kevintlu.com">me@kevintlu.com</a>. if
-        you&apos;re in tech & interested in doing an interview, reach out!
-      </p>
+    <div className="min-h-screen flex py-12 sm:py-16 lg:py-32 px-5 justify-center font-serif">
+      <div>
+        <h1>hi im kevin 👋...more is coming.</h1>
+        <p>
+          message me at{" "}
+          <a className="text-blue-500 underline" href="mailto:me@kevintlu.com">
+            me@kevintlu.com
+          </a>
+          . if you&apos;re in tech & interested in doing an interview, reach
+          out!
+        </p>
+      </div>
     </div>
   );
 }
