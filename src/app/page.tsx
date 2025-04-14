b10d556f5190a0d68bebd35963a38eefc7a7dc09
Kevin Lu <kevinlu.email@gmail.com>
2025-04-13 20:44:18 -0700

    add email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="font-serif">hi im kevin. more is coming.</h1>
+      <p>
+        message me at <a href="mailto:me@kevintlu.com">me@kevintlu.com</a>. if
+        you&apos;re in tech & interested in doing an interview, reach out!
+      </p>
     </div>
   );
 }
